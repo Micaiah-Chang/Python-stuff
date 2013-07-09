126b5ff10f6a4bedbd2e321cb0b0e50837c084ae
Micaiah-Chang <Micaiah@server.fake>
2013-07-08 19:24:02 -0700

    Finished part 3 of the Django tutorial whooo

--- project/foo/foo/urls.py
+++ project/foo/foo/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^polls/' include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
     # Examples:
     # url(r'^$', 'foo.views.home', name='home'),
     # url(r'^foo/', include('foo.foo.urls')),
@@ -13,5 +16,3 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
